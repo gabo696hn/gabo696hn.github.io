33a42208009c7f6633b6c53f1d446bc9fe659bc1
Gabo HN <gabo696hn@gmail.com>
2018-02-07 22:02:39 -0400

    added object

---
+++ js/app.js
@@ -0,0 +1,1 @@
+var gabohn = angular.module('gabohn', []);
---
+++ js/data.js
@@ -0,0 +1,40 @@
+const title = 'Welcome';
+const paragraphs = [];
+paragraphs.push('Greetings, my name is Gabriel and I’m a systems engineer passionate about data analysis and web application development.');
+paragraphs.push('I’ve worked with the computational tool R and the KDD methodology. I’m currently studying other tools such as python and svg charts, intending to amplify my possibilities.');
+paragraphs.push('I’m also studying the languages associated with web, since it seems to me that currently is the best way to present information.');
+// console.log(paragraphs);
+const imgsrc = [];
+imgsrc.push('img/home/1.svg');
+imgsrc.push('img/home/2.svg');
+imgsrc.push('img/home/3.svg');
+// console.log(imgsrc);
+const descriptions = [];
+descriptions.push('this is the img 1');
+descriptions.push('this is the img 2');
+descriptions.push('this is the img 3');
+const images = [];
+
+for (let index = 0; index < imgsrc.length; index++) {
+    images.push({
+        src: imgsrc[index],
+        desc: descriptions[index]
+    })
+}
+
+const imagery = {
+    descriptions: descriptions,
+    src: imgsrc
+};
+
+const subsection = {
+    title: title,
+    paragraphs: paragraphs,
+    imagery: images
+};
+
+//console.log(subsection.imagery);
+
+const welcome = JSON.stringify(subsection);
+
+console.log(welcome)
