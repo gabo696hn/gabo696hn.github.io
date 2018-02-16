b050b4c504337157b5b1634f4034418d1b6874a2
Gabo HN <gabo696hn@gmail.com>
2018-02-16 19:49:01 -0400

    version 1.0.0

--- js/controllers/ContactController.js
+++ js/controllers/ContactController.js
@@ -1,4 +1,16 @@
 app.controller('ContactController', ['$scope', function($scope) {
     $scope.banner = 'img/contact.svg';
+    $scope.data = {
+        en: {
+            mail: 'Your email',
+            message: 'Your message',
+            send: 'Send'
+        },
+        es: {
+            mail: 'Tu correo electrónico',
+            message: 'Tu mensaje',
+            send: 'Enviar'
+        }
+    }
 }]);
 
